3db50b7dec4b483bc3e6a29f9e4f261de4834021
sateesh.g <sateesh.g@go-jek.com>
2019-12-30 23:11:58 +0530

    Added more column names for contact table

---
+++ resources/migrations/002-add-basic-columns-to-contacts.down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE contact DROP COLUMN firstName;
+ALTER TABLE contact DROP COLUMN lastName;
+ALTER TABLE contact DROP COLUMN phoneNumber;
+ALTER TABLE contact DROP COLUMN email;
+ALTER TABLE contact DROP COLUMN favourite;
+
---
+++ resources/migrations/002-add-basic-columns-to-contacts.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE contact ADD COLUMN firstName text;
+ALTER TABLE contact ADD COLUMN lastName text;
+ALTER TABLE contact ADD COLUMN phoneNumber text;
+ALTER TABLE contact ADD COLUMN email text;
+ALTER TABLE contact ADD COLUMN favourite bool;
