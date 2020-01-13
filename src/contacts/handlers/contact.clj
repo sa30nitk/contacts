33cc8f168f28548fff241ca94fe0b1833744d5db
sateesh.g <sateesh.g@go-jek.com>
2020-01-13 12:38:58 +0530

    Added try catch block on db operations in contacts namespace

--- src/contacts/handlers/contact.clj
+++ src/contacts/handlers/contact.clj
@@ -8,13 +8,25 @@
 
 (defn get-contact-handler
   [{:keys [route-params]}]
-  (let [contact (j/get-by-id config/pg-db :contact (:id route-params))]
-    (if (not= contact nil) (res/response contact) (res/not-found (str "Contact not found")))))
+  ((try
+     (let [contact (j/get-by-id config/pg-db :contact (:id route-params))]
+       (if (not= contact nil) (res/response contact) (res/not-found (str "Contact not found"))))
+     (catch Exception e
+       (.printStackTrace e)
+       {:status  500
+        :headers {}
+        :body    {:error "internal server error"}}))))
 
 (defn delete-contact-handler
   [{:keys [route-params]}]
-  (let [isDeleted (j/delete! config/pg-db :contact ["id = ?" (:id route-params)])]
-    (if (= isDeleted (list 1)) (res/response "Successfully deleted") (res/not-found (str "Contact not found")))))
+  (try
+    (let [isDeleted (j/delete! config/pg-db :contact ["id = ?" (:id route-params)])]
+      (if (= isDeleted (list 1)) (res/response "Successfully deleted") (res/not-found (str "Contact not found"))))
+    (catch Exception e
+      (.printStackTrace e)
+      {:status  500
+       :headers {}
+       :body    {:error "internal server error"}})))
 
 (defn create-contact-handler
   [request]
@@ -27,17 +39,31 @@
       (res/created (str (name (:scheme request)) "://" (:host (walk/keywordize-keys (:headers request))) "/v1/contacts/" (:id contactWithUUID)))
       (catch Exception e
         (.printStackTrace e)
-        (res/response "Internal server error")))))
+        {:status  500
+         :headers {}
+         :body    {:error "internal server error"}}))))
 
 (defn get-contacts-handler [request]
-  (let [contacts (j/query config/pg-db ["select * from contact"])]
-    (if (not= contacts nil) (res/response contacts) (res/response {}))))
+  ((try
+     (let [contacts (j/query config/pg-db ["select * from contact"])]
+       (if (not= contacts nil) (res/response contacts) (res/response {})))
+     (catch Exception e
+       (.printStackTrace e)
+       {:status  500
+        :headers {}
+        :body    {:error "internal server error"}}))))
 
 (defn patch-contact-handler
   [request]
-  (let [body           (request :body)
-        keywordizeBody (walk/keywordize-keys body)
-        id             (:id keywordizeBody)
-        contact        (select-keys keywordizeBody [:firstName :lastName :phoneNumber :email :favourite])
-        isUpdated      (j/update! config/pg-db :contact contact ["id = ?" id])]
-    (if (= isUpdated (list 1)) (res/response "Successful update") (res/not-found "Update failed"))))
+  ((try
+     (let [body           (request :body)
+           keywordizeBody (walk/keywordize-keys body)
+           id             (:id keywordizeBody)
+           contact        (select-keys keywordizeBody [:firstName :lastName :phoneNumber :email :favourite])
+           isUpdated      (j/update! config/pg-db :contact contact ["id = ?" id])]
+       (if (= isUpdated (list 1)) (res/response "Successful update") (res/not-found "Update failed")))
+     (catch Exception e
+       (.printStackTrace e)
+       {:status  500
+        :headers {}
+        :body    {:error "internal server error"}}))))
