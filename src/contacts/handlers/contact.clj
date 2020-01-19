279ab48ef3a99ec5fec50c837d46e176473322b0
sateesh.g <sateesh.g@go-jek.com>
2020-01-19 16:08:01 +0530

    Using connection pool for all requests

--- src/contacts/handlers/contact.clj
+++ src/contacts/handlers/contact.clj
@@ -9,25 +9,26 @@
 (defn get-contact-handler
   [{:keys [route-params]}]
   (try
-    (let [contact (j/get-by-id config/pg-db :contact (:id route-params))]
-      (if (not= contact nil) (res/response contact) (res/not-found (str "Contact not found"))))
+    (j/with-db-connection [conn {:datasource @config/datasource}]
+                          (let [contact (j/get-by-id conn :contact (:id route-params))]
+                            (if (not= contact nil) (res/response contact) (res/not-found (str "Contact not found")))))
     (catch Exception e
       (.printStackTrace e)
       {:status  500
        :headers {}
-       :body    {:error "internal server error"}}))
+       :body    {:error "internal server error"}})))
 
-
-  (defn delete-contact-handler
-    [{:keys [route-params]}]
-    (try
-      (let [isDeleted (j/delete! config/pg-db :contact ["id = ?" (:id route-params)])]
-        (if (= isDeleted (list 1)) (res/response "Successfully deleted") (res/not-found (str "Contact not found"))))
-      (catch Exception e
-        (.printStackTrace e)
-        {:status  500
-         :headers {}
-         :body    {:error "internal server error"}}))))
+(defn delete-contact-handler
+  [{:keys [route-params]}]
+  (try
+    (j/with-db-connection [conn {:datasource @config/datasource}]
+                          (let [isDeleted (j/delete! config/pg-db :contact ["id = ?" (:id route-params)])]
+                            (if (= isDeleted (list 1)) (res/response "Successfully deleted") (res/not-found (str "Contact not found")))))
+    (catch Exception e
+      (.printStackTrace e)
+      {:status  500
+       :headers {}
+       :body    {:error "internal server error"}})))
 
 (defn create-contact-handler
   [request]
@@ -36,7 +37,8 @@
         contact         (select-keys keywordizeBody [:firstName :lastName :phoneNumber :email :favourite])
         contactWithUUID (merge contact {:id (uuid)})]
     (try
-      (j/insert-multi! config/pg-db :contact [contactWithUUID])
+      (j/with-db-connection [conn {:datasource @config/datasource}]
+                            (j/insert-multi! conn :contact [contactWithUUID]))
       (res/created (str (name (:scheme request)) "://" (:host (walk/keywordize-keys (:headers request))) "/v1/contacts/" (:id contactWithUUID)))
       (catch Exception e
         (.printStackTrace e)
@@ -50,22 +52,23 @@
                           (let [contacts (j/query conn ["select * from contact"])]
                             (if (not= contacts nil) (res/response contacts) (res/response {}))))
     (catch Exception e
-       (.printStackTrace e)
-       {:status  500
-        :headers {}
-        :body    {:error "internal server error"}})))
+      (.printStackTrace e)
+      {:status  500
+       :headers {}
+       :body    {:error "internal server error"}})))
 
 (defn patch-contact-handler
   [request]
   (try
-     (let [body           (request :body)
-           keywordizeBody (walk/keywordize-keys body)
-           id             (:id keywordizeBody)
-           contact        (select-keys keywordizeBody [:firstName :lastName :phoneNumber :email :favourite])
-           isUpdated      (j/update! config/pg-db :contact contact ["id = ?" id])]
-       (if (= isUpdated (list 1)) (res/response "Successful update") (res/not-found "Update failed")))
-     (catch Exception e
-       (.printStackTrace e)
-       {:status  500
-        :headers {}
-        :body    {:error "internal server error"}})))
+    (j/with-db-connection [conn {:datasource @config/datasource}]
+                          (let [body           (request :body)
+                                keywordizeBody (walk/keywordize-keys body)
+                                id             (:id keywordizeBody)
+                                contact        (select-keys keywordizeBody [:firstName :lastName :phoneNumber :email :favourite])
+                                isUpdated      (j/update! conn :contact contact ["id = ?" id])]
+                            (if (= isUpdated (list 1)) (res/response "Successful update") (res/not-found "Update failed"))))
+    (catch Exception e
+      (.printStackTrace e)
+      {:status  500
+       :headers {}
+       :body    {:error "internal server error"}})))
